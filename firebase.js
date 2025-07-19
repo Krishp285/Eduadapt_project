59f18815916b7a0aa0ed2af7d3ec67bc8387a510
krishp285 <krishkpatelk2005@gmail.com>
2025-07-19 17:06:13 +0530

    first commit

--- firebase.js
+++ firebase.js
@@ -1,11 +1,3 @@
-const firebaseConfig = {
-  authDomain: "eduadapt-8e62e.firebaseapp.com",
-  projectId: "eduadapt-8e62e",
-  storageBucket: "eduadapt-8e62e.firebasestorage.app",
-  messagingSenderId: "1002156798282",
-  appId: "1:1002156798282:web:c51e9231ae08d268c8d1b3",
-  measurementId: "G-74W748KTRP"
-};
   //export const firebaseConfig; // Ensure this is exported if using modules
 
   // Replace with your Firebase project credentials
