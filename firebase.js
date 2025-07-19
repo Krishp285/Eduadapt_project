d22560f99dcbb0c2d9b69c79cf19ee655f04ff7b
krishp285 <krishkpatelk2005@gmail.com>
2025-07-19 18:07:45 +0530

    first commit

--- firebase.js
+++ firebase.js
@@ -1,7 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "./firebase-config";
+const firebaseConfig = {
+  apiKey: "AIzaSyC_Xw8FWG04xpxQXyrYDkQK5u82_yCYp0w",
+  authDomain: "eduadapt-8e62e.firebaseapp.com",
+  projectId: "eduadapt-8e62e",
+  storageBucket: "eduadapt-8e62e.firebasestorage.app",
+  messagingSenderId: "1002156798282",
+  appId: "1:1002156798282:web:c51e9231ae08d268c8d1b3",
+  measurementId: "G-74W748KTRP"
+};
+
 
 const app = initializeApp(firebaseConfig);
 
